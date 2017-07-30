2573531e69be250740889dba791dfa6e9c892823
Maxislav <lmaxim@mail.ru>
2017-07-30 23:07:28 +0300

    css module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TitleComponent from './component/title-component/title-component';
-import SkillHistoryComponent from './component/slill-history-component/slill-history-component'
-import appStyl from './app.styl'
+import SkillHistoryComponent from './component/slill-history-component/slill-history-component';
+import appStyl from './app.styl';
 
 export default class App extends React.Component{
     render(){
