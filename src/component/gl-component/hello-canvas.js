97fcfde2e7f81f5a682c01e60a28a82f386ead1b
Maxim Lipatov <lmaxim@mail.ru>
2017-10-25 15:37:45 +0300

    hello gl point1

--- src/component/gl-component/hello-canvas.js
+++ src/component/gl-component/hello-canvas.js
@@ -9,16 +9,21 @@ export default class HelloCanvas{
 
 
     //Координаты и размер точки
-    const VSHADER_SOURCE =
-       'void main() {\n' +
-      'gl_Position = vec4(0.0, 0.5, 0.0, 1.0);' + // Координаты
-     ' gl_PointSize = 10.0;' +
-     '}';
+    const VSHADER_SOURCE =`
+      attribute vec4 a_Position;
+      attribute float a_PointSize;
+      
+      void main() {\n 
+      //gl_Position = vec4(0.0, 0.5, 0.0, 1.0);  // Координаты
+      gl_Position = a_Position;  // Координаты
+      //gl_PointSize = 10.0; 
+      gl_PointSize = a_PointSize; 
+     }`;
 
     const FSHADER_SOURCE =
-       'void main() {' +
-     ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);' + // Установить цвет
-     '}';
+     `void main() { 
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);  // Установить цвет
+     }`;
 
 
     const vShader = createShader(gl, gl.VERTEX_SHADER, VSHADER_SOURCE);
@@ -27,6 +32,26 @@ export default class HelloCanvas{
     const program = createProgram(gl, vShader, fShader)
     gl.useProgram(program)
     gl.drawArrays(gl.POINTS, 0, 1);
+
+    var a_Position = gl.getAttribLocation(program, 'a_Position');
+
+    var a_PointSize = gl.getAttribLocation(program, 'a_PointSize')
+
+    var  position = new Float32Array([0.5, 0.0, 0.0, 1.0]);
+    var size = new Float32Array([100.0])
+
+   // gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0); //устанавливает значение отдельных элементов
+    gl.vertexAttrib4fv(a_Position, position);
+
+    //gl.vertexAttrib1f(a_PointSize, 100.0);
+    gl.vertexAttrib1fv(a_PointSize, size);
+
+
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+
+    //console.log(gl.getAttribLocation(program, 'a_Position'))
 
   }
 }
