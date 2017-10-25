f752aa58fa872286ffe5919e5f0f0199560548de
Maxim Lipatov <lmaxim@mail.ru>
2017-10-25 18:24:36 +0300

    hello gl point3 (dots color)

--- src/component/gl-component/hello-canvas.js
+++ src/component/gl-component/hello-canvas.js
@@ -1,9 +1,11 @@
-import {getWebGLContext, createShader, createProgram} from "./web-gL-context";
+import {getWebGLContext, createShader, createProgram} from "./web-gL";
 
 
 function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+  var vertexXYColor = new Float32Array([
+    -0.5, 0.5, 1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.0, 1.0, 0.0,
+    0.5, -0.5, 0.0, 0.0, 1.0,
   ]);
   var n = 3;
 
@@ -15,12 +17,21 @@ function initVertexBuffers(gl) {
   // Определить тип буферного объекта
    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
    // Записать данные в буферный объект
-   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+   gl.bufferData(gl.ARRAY_BUFFER, vertexXYColor, gl.STATIC_DRAW);
+
+  var FSIZE = vertexXYColor.BYTES_PER_ELEMENT;
+
+  console.log(FSIZE)
+
+
   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  // Сохранить ссылку на буферный объект в переменной a_Position
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-  // Разрешить присваивание переменной a_Position
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE*5, 0);
   gl.enableVertexAttribArray(a_Position);
+
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE*5, FSIZE*2);
+  gl.enableVertexAttribArray(a_Color);
+
   return n;
 }
 
@@ -34,21 +45,25 @@ export default class HelloCanvas{
     //Координаты и размер точки
     const VSHADER_SOURCE =`
       attribute vec4 a_Position;
-      attribute float a_PointSize;
+      attribute vec4 a_Color;
+      varying vec4 v_Color;
       
       void main() {\n 
       //gl_Position = vec4(0.0, 0.5, 0.0, 1.0);  // Координаты
       gl_Position = a_Position;  // Координаты
-      //gl_PointSize = 10.0; 
-      gl_PointSize = a_PointSize; 
+      gl_PointSize = 10.0; 
+      v_Color = a_Color; // Передача данных во фрагментный шейдер
      }`;
 
     const FSHADER_SOURCE =`
      precision mediump float;
      uniform vec4 u_FragColor;
+     varying vec4 v_Color;
      void main() { 
       //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);  // Установить цвет
-      gl_FragColor = u_FragColor;  // Установить цвет
+      //gl_FragColor = u_FragColor;  // Установить цвет
+      gl_FragColor = v_Color;  // Установить цвет
+      
      }`;
 
 
@@ -59,33 +74,19 @@ export default class HelloCanvas{
 
     gl.useProgram(program)
     gl.program = program;
-    gl.drawArrays(gl.POINTS, 0, 1);
-
-    var a_Position = gl.getAttribLocation(program, 'a_Position');
-
-    var a_PointSize = gl.getAttribLocation(program, 'a_PointSize')
-
-    var  position = new Float32Array([0.5, 0.0, 0.0, 1.0]);
-    var size = new Float32Array([5.0])
-
-   // gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0); //устанавливает значение отдельных элементов
-    gl.vertexAttrib4fv(a_Position, position);
-
-    //gl.vertexAttrib1f(a_PointSize, 100.0);
-    gl.vertexAttrib1fv(a_PointSize, size);
 
 
-    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-    var color = new Float32Array([0,1,1,1])
-    //gl.uniform4f(u_FragColor,0,1,0,1)
-    gl.uniform4fv(u_FragColor,color)
+
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     //gl.drawArrays(gl.POINTS, 0, 1);
 
 
     const n = initVertexBuffers(gl)
+    console.log(n)
     gl.drawArrays(gl.POINTS, 0, n);
+
+
 
 
     //console.log(gl.getAttribLocation(program, 'a_Position'))
