b17b7eb80a13c5fbab393d2ddbfa4bd38c2eeab3
maxislav <maximmalyshyn@gmail.com>
2018-01-23 23:58:56 +0200

    ruller

--- src/component/skill-history-component/skill-history-ruler-component/skill-history-ruler-component.jsx
+++ src/component/skill-history-component/skill-history-ruler-component/skill-history-ruler-component.jsx
@@ -10,7 +10,14 @@ import {getMinDate} from '../asset'
 export default class SkillHistoryRulerComponent extends Component{
 	render(){
 		if(this.props.history.length){
-		console.log(getMinDate(this.props.history))			
+		//console.log(getMinDate(this.props.history))
+			let dateMin = getMinDate(this.props.history).getTime()
+			const dateNow = new Date().getTime()
+			while(new Date(dateMin+=(24*3600*1000)).getTime() < dateNow){
+				if(new Date(dateMin).getDate() == 1 && new Date(dateMin).getMonth() == 0){
+					console.log(new Date(dateMin))
+				}
+			}
 		}
 		
 
