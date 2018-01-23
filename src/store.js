8796b4c84652b5aec90faff50f1b003b42212d32
Maxim Lipatov <lmaxim@mail.ru>
2018-01-23 17:27:42 +0200

    add period

--- src/store.js
+++ src/store.js
@@ -1,13 +1,16 @@
 import {createStore, applyMiddleware} from "redux";
-import skillReducer from './reduscers/skill-reducer'
 import reducers from './reduscers/redusers';
 import promise from 'redux-promise-middleware'
 import thunk from 'redux-thunk';
-import {createLogger} from "redux-logger";
-const logger = createLogger();
-const middleware =  applyMiddleware(promise(), thunk, logger);
+let middleware;
 
+
+if(NODE_ENV == 'dev'){
+  const {createLogger} = require('redux-logger')
+  const logger = createLogger();
+  middleware =  applyMiddleware(promise(), thunk, logger);
+}else {
+  middleware =  applyMiddleware(promise(), thunk);
+}
 const store = createStore( reducers, middleware );
-
-
 export default store
