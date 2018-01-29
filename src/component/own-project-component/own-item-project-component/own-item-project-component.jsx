7831f94bff6f05db0571c3280809ab1ee7e131f9
Maxim Lipatov <lmaxim@mail.ru>
2018-01-29 12:32:30 +0200

    fill own pr

--- src/component/own-project-component/own-item-project-component/own-item-project-component.jsx
+++ src/component/own-project-component/own-item-project-component/own-item-project-component.jsx
@@ -15,26 +15,35 @@ export class OwnItemProjectComponent extends Component{
     const {data} = this.props
    // console.log(data)
     return (
-      <div>
+      <div className={styl.component}>
         <div className={styl.bookmark}>
           <div>
             <div>
               <a href={data.play_url} target="_blank">
-                <img src="./img/play_prism_hlock.png"/>
+                <h2>{data.name}</h2>
               </a>
-
             </div>
           </div>
         </div>
         <div className={styl.overflow} style={{padding: "2px 8px 8px 8px"}}>
           <div className={styl.content}>
+            <img className={styl.mainImage} src={data.mainImg}/>
             <img className={styl.ico} src={data.ico}/>
-            <h2>
-              {data.name}
-            </h2>
             <div>
               {data.description}
             </div>
+            <div className={styl.play_prism}>
+              <a href={data.play_url} target="_blank" >
+                <img src="./img/play_prism_hlock.png"/>
+              </a>
+
+            </div>
+            <div className={styl.imgs} >
+              {data.imgs.map((item,index )=>(
+                <img src={item} key={index}/>
+              ))}
+            </div>
+
             <div className={styl.clear_both}>
 
             </div>
