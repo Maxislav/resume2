45518dfbdd975ed2e105f32ed2c87380258e4340
Maxim Lipatov <lmaxim@mail.ru>
2018-02-22 17:22:30 +0200

    modal success

--- src/component/contact-component/contact-info-component/contact-info-component.jsx
+++ src/component/contact-component/contact-info-component/contact-info-component.jsx
@@ -3,6 +3,7 @@ import styl from  './contact-info-component.styl'
 import {xhrGet} from "../../../asset/xhr-get";
 
 const $contact = []
+const $education = []
 
 
 const A = ({text}) =>{
@@ -18,7 +19,10 @@ export class ContactInfoComponent extends React.Component{
 
   constructor(...args){
     super(...args)
-    this.state = {contact: $contact};
+    this.state = {
+      contact: $contact,
+      education: $education
+    };
 
     if(!$contact.length){
       xhrGet('./cv-json-data/contact.json', 'json')
@@ -28,35 +32,57 @@ export class ContactInfoComponent extends React.Component{
             contact
           })
         })
+      xhrGet('./cv-json-data/education.json', 'json')
+        .then(contact => {
+          contact.map(item=>$education.push(item))
+          this.setState({
+            education: $education
+          })
+        })
     }
 
   }
 
   render() {
     return (
-      <table className={styl['contact-table']}>
-        <tbody>
-        {
-          this.state.contact.map((contact, index) => {
-            return <tr key={index}>
-              {
+      <div>
 
-                contact.map((item, index2) => {
-                  if (index == 0) {
-                    return <td colSpan={contact.length == 2 ? 1 : 2} key={index2} className={styl['my-name']}>
-                      {item}
+        <table className={styl['contact-table']}>
+          <tbody>
+          {
+            this.state.contact.map((contact, index) => {
+              return <tr key={index}>
+                {
+
+                  contact.map((item, index2) => {
+                    if (index == 0) {
+                      return <td colSpan={contact.length == 2 ? 1 : 2} key={index2} className={styl['my-name']}>
+                        {item}
+                      </td>
+                    }
+                    return <td key={index2}>
+                      <A text={item}/>
                     </td>
-                  }
-                  return <td key={index2}>
-                    <A text={item}/>
-                  </td>
-                })
-              }
-            </tr>
-          })
-        }
-        </tbody>
-      </table>
+                  })
+                }
+              </tr>
+            })
+          }
+          </tbody>
+        </table>
+        <div className={styl.education}>
+          <h3>
+            Education
+          </h3>
+          <div>
+            {this.state.education.map((it, index)=>(<div key={index}>{it}</div>))}
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+
     )
   }
 }
