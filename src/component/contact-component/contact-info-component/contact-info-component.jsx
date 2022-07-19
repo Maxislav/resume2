942175105f45737cc2df8a21b3ccf04610296114
Maxim Lipatov <maximmalyshyn@gmail.com>
2022-07-19 13:37:50 +0300

    update epam2

--- src/component/contact-component/contact-info-component/contact-info-component.jsx
+++ src/component/contact-component/contact-info-component/contact-info-component.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import styl from  './contact-info-component.styl'
+import styl from './contact-info-component.styl'
 import {xhrGet} from "../../../asset/xhr-get";
 import {NgIfComponent} from "../../ng-If-component/ng-If-component";
 import {autobind} from "core-decorators";
@@ -8,90 +8,93 @@ const $contact = []
 const $education = []
 
 
-const A = ({text}) =>{
-  const matches = text.match(/^https?/)
-  if(matches){
-    return (<a href={text} target='_blank'>{text}</a>)
-  }
-  return (<span>{text}</span>)
+const A = ({text}) => {
+    const matches = text.match(/^https?/)
+    if (matches) {
+        return (<a href={text} target='_blank'>{text}</a>)
+    }
+    return (<span>{text}</span>)
 }
 
 
-export class ContactInfoComponent extends React.Component{
+export class ContactInfoComponent extends React.Component {
 
-  constructor(...args){
-    super(...args)
-    this.state = {
-      contact: $contact,
-      education: $education,
-      showOlolo: true
-    };
+    constructor(...args) {
+        super(...args)
+        this.state = {
+            contact: $contact,
+            education: $education,
+            showOlolo: true
+        };
 
-    if(!$contact.length){
-      xhrGet('./cv-json-data/contact.json', 'json')
-        .then(contact => {
-          contact.map(item=>$contact.push(item))
-          this.setState({
-            contact
-          })
-        })
-      xhrGet('./cv-json-data/education.json', 'json')
-        .then(contact => {
-          contact.map(item=>$education.push(item))
-          this.setState({
-            education: $education
-          })
+        if (!$contact.length) {
+            xhrGet('./cv-json-data/contact.json', 'json')
+                .then(contact => {
+                    $contact.length = 0;
+                    contact.map(item => $contact.push(item))
+                    this.setState({
+                        contact
+                    })
+                })
+            xhrGet('./cv-json-data/education.json', 'json')
+                .then((contact) => {
+                    $education.length = 0
+                    contact.map(item => $education.push(item))
+                    this.setState({
+                        education: $education
+                    })
+                })
+        }
+    }
+
+    @autobind
+    onHide() {
+        this.setState({
+            showOlolo: !this.state.showOlolo
         })
     }
-  }
 
-  @autobind
-  onHide(){
-    this.setState({
-      showOlolo: !this.state.showOlolo
-    })
-  }
+    render() {
+        return (
+            <div>
 
-  render() {
-    return (
-      <div>
+                <table className={styl['contact-table']}>
+                    <tbody>
+                    {
+                        this.state.contact.map((contact, index) => {
+                            return <tr key={index}>
+                                {
 
-        <table className={styl['contact-table']}>
-          <tbody>
-          {
-            this.state.contact.map((contact, index) => {
-              return <tr key={index}>
-                {
-
-                  contact.map((item, index2) => {
-                    if (index == 0) {
-                      return <td colSpan={contact.length == 2 ? 1 : 2} key={index2} className={styl['my-name']}>
-                        {item}
-                      </td>
+                                    contact.map((item, index2) => {
+                                        if (index == 0) {
+                                            return <td colSpan={contact.length == 2 ? 1 : 2} key={index2}
+                                                       className={styl['my-name']}>
+                                                {item}
+                                            </td>
+                                        }
+                                        return <td key={index2}>
+                                            <A text={item}/>
+                                        </td>
+                                    })
+                                }
+                            </tr>
+                        })
                     }
-                    return <td key={index2}>
-                      <A text={item}/>
-                    </td>
-                  })
-                }
-              </tr>
-            })
-          }
-          </tbody>
-        </table>
-        <div className={styl.education}>
-          <h3>
-            Education
-          </h3>
-          <div>
-            {this.state.education.map((it, index)=>(<div key={index}>{it}</div>))}
-          </div>
-        </div>
-        <div>
+                    </tbody>
+                </table>
+                <div className={styl.education}>
+                    <h3>
+                        Education
+                    </h3>
+                    <div>
+                        {this.state.education.map((it, index) => (<div key={index}>{it}</div>))}
+                    </div>
+                </div>
+                <div>
 
-        </div>
-      </div>
+                </div>
+            </div>
 
-    )
-  }
+        )
+    }
 }
